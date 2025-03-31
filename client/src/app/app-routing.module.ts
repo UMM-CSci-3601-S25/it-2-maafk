17d1808c85374721af34f6b3b0934b744d2535d1
Mahathir <mosta044@umn.edu>
2025-03-31 11:55:30 -0500

    Changes
    Made some changes to various files to add more routes, add a file for rounds,etc

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,14 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { AddUserComponent } from './users/add-user.component';
-import { UserListComponent } from './users/user-list.component';
-import { UserProfileComponent } from './users/user-profile.component';
-import { CompanyListComponent } from './company-list/company-list.component';
 import { HostComponent } from './hosts/host-list.component';
 import { AddPlayerComponent } from './players/player-list.component';
 import { HostSettingsComponent } from './hosts/host-setting.component';
 import { GameComponent } from './game/game.component';
+import { JoinComponent } from './join/join.component';
 
 
 // Note that the 'users/new' route needs to come before 'users/:id'.
@@ -21,13 +18,13 @@ const routes: Routes = [
   {path: 'prompts', component: HostSettingsComponent, title: 'Host Settings'},
   {path: 'host/settings', component: HostSettingsComponent, title: 'Host Settings'},
 
+  {path: 'games', component: HomeComponent},
+  {path: 'games/join', component: JoinComponent, title: "PRAGmatic"},
+  {path: 'games/new', component: HostComponent, title: "PRAGmatic"},
+  {path: 'games/:id', component: GameComponent, title: "Game"},
+
   {path: 'game', component: GameComponent, title: 'Game'},
   {path: 'game/temporary', component: GameComponent, title: 'Temporary Game'},
-
-  {path: 'users', component: UserListComponent, title: 'Users'},
-  {path: 'users/new', component: AddUserComponent, title: 'Add User'},
-  {path: 'users/:id', component: UserProfileComponent, title: 'User Profile'},
-  {path: 'companies', component: CompanyListComponent, title: 'Companies'}
 ];
 
 @NgModule({
