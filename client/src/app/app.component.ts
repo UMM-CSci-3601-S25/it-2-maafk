1604d19c409c01ff739bb26eb6555fe6200c17f7
Mahathir <mosta044@umn.edu>
2025-04-06 20:45:23 -0500

    Changed App component
    Minor changes to the app component

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -13,5 +13,5 @@ import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
   imports: [MatSidenavModule, MatToolbarModule, MatListModule, RouterLink, RouterLinkActive, MatIconModule, MatButtonModule, RouterOutlet]
 })
 export class AppComponent {
-  title = 'humanity against apples';
+  title = 'humanity against appels';
 }
