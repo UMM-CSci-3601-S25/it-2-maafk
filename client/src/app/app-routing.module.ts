6a68ea393f6e1eb68d2dfc40d5c2ba8ff20f74b5
Mahathir <mosta044@umn.edu>
2025-04-06 20:44:52 -0500

    Made changes
    Made changes to the app routing module.

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,9 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { HostComponent } from './hosts/host-list.component';
-import { AddPlayerComponent } from './players/player-list.component';
-import { HostSettingsComponent } from './hosts/host-setting.component';
+import { HostComponent } from './hosts/host-setting.component';
 import { GameComponent } from './game/game.component';
 import { JoinComponent } from './join/join.component';
 
@@ -13,18 +11,13 @@ import { JoinComponent } from './join/join.component';
 // 'users/new'; the router would just think that the string 'new' is a user ID.
 const routes: Routes = [
   {path: '', component: HomeComponent, title: 'Home'},
-  {path: 'host', component: HostComponent, title: 'Host'},
-  {path: 'player', component: AddPlayerComponent, title: 'Player'},
-  {path: 'prompts', component: HostSettingsComponent, title: 'Host Settings'},
-  {path: 'host/settings', component: HostSettingsComponent, title: 'Host Settings'},
-
   {path: 'games', component: HomeComponent},
-  {path: 'games/join', component: JoinComponent, title: "PRAGmatic"},
-  {path: 'games/new', component: HostComponent, title: "PRAGmatic"},
+  {path: 'games/join', component: JoinComponent, title: "Join"},
+  {path: 'games/new', component: HostComponent, title: "New game"},
   {path: 'games/:id', component: GameComponent, title: "Game"},
+  {path: 'prompts', component: HostComponent, title: 'Prompt'},
 
-  {path: 'game', component: GameComponent, title: 'Game'},
-  {path: 'game/temporary', component: GameComponent, title: 'Temporary Game'},
+
 ];
 
 @NgModule({
@@ -32,4 +25,3 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
-
