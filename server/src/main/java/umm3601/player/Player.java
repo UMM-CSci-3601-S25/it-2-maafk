68c2e217812e15df9000890d1c3af5c8d216d088
Mahathir <mosta044@umn.edu>
2025-04-06 20:50:01 -0500

    Player java
    Commented out

--- server/src/main/java/umm3601/player/Player.java
+++ server/src/main/java/umm3601/player/Player.java
@@ -1,36 +1,36 @@
-package umm3601.player;
+// package umm3601.player;
 
-import org.mongojack.Id;
-import org.mongojack.ObjectId;
+// import org.mongojack.Id;
+// import org.mongojack.ObjectId;
 
 
 
-@SuppressWarnings({"VisibilityModifier"})
-public class Player {
+// @SuppressWarnings({"VisibilityModifier"})
+// public class Player {
 
-  @ObjectId @Id
-  @SuppressWarnings({"MemberName"})
-  public String _id;
+//   @ObjectId @Id
+//   @SuppressWarnings({"MemberName"})
+//   public String _id;
 
-  public String name;
+//   public String name;
 
-  @Override
-  public boolean equals(Object obj) {
-    if (!(obj instanceof Player)) {
-      return false;
-    }
-    Player other = (Player) obj;
-    return _id.equals(other._id);
-  }
+//   @Override
+//   public boolean equals(Object obj) {
+//     if (!(obj instanceof Player)) {
+//       return false;
+//     }
+//     Player other = (Player) obj;
+//     return _id.equals(other._id);
+//   }
 
-  @Override
-  public int hashCode() {
-    return _id.hashCode();
-  }
+//   @Override
+//   public int hashCode() {
+//     return _id.hashCode();
+//   }
 
-  @Override
-  public String toString() {
-    return name;
-  }
+//   @Override
+//   public String toString() {
+//     return name;
+//   }
 
-}
+// }
