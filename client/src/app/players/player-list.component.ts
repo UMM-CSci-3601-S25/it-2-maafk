70e61cb616e4a1ae411a923cf24f3a62a57fe87c
Mahathir <mosta044@umn.edu>
2025-04-06 20:49:03 -0500

    Player component
    Commented out these files since we wont need them.

--- client/src/app/players/player-list.component.ts
+++ client/src/app/players/player-list.component.ts
@@ -1,135 +1,132 @@
-import { Component } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { FormsModule } from '@angular/forms';
-import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
-import { MatOptionModule } from '@angular/material/core';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatGridListModule } from '@angular/material/grid-list';
-import { MatIconModule } from '@angular/material/icon';
-import { MatInputModule } from '@angular/material/input';
-import { MatListModule } from '@angular/material/list';
-import { MatRadioModule } from '@angular/material/radio';
-import { MatSelectModule } from '@angular/material/select';
-import { MatTooltipModule } from '@angular/material/tooltip';
-import { RouterLink } from '@angular/router';
-import { MatSnackBar } from '@angular/material/snack-bar';
-import { PlayerService } from './player.service';
-import { Router } from '@angular/router';
+// import { Component } from '@angular/core';
+// import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+// import { MatButtonModule } from '@angular/material/button';
+// import { MatCardModule } from '@angular/material/card';
+// import { MatFormFieldModule } from '@angular/material/form-field';
+// import { MatInputModule } from '@angular/material/input';
+// import { GameService } from '../game/game.service';
+// import { MatSnackBar } from '@angular/material/snack-bar';
+// import { Router } from '@angular/router';
+// import { WebSocketService } from '../game/web-socket.service';
 
+// @Component({
+//   selector: 'app-join',
+//   imports:
+//     [
+//       FormsModule,
+//       ReactiveFormsModule,
+//       MatCardModule,
+//       MatFormFieldModule,
+//       MatInputModule,
+//       MatButtonModule
+//     ],
+//   templateUrl: './join.component.html',
+//   styleUrl: './join.component.scss'
+// })
+// export class JoinComponent {
 
+//   joinGameForm = new FormGroup({
+//     // We allow alphanumeric input and limit the length for a game id.
+//     gameId: new FormControl('', Validators.compose([
+//       Validators.required,
+//       Validators.minLength(24),
+//       // length of the game id will be 24
+//       Validators.maxLength(24),
+//       Validators.pattern('^[A-Fa-f0-9]{24}$')
+//     ])),
+//     playerName: new FormControl('', Validators.compose([
+//       Validators.required,
+//       Validators.minLength(2),
+//       // length of the player name must be 2-100 characters
+//       Validators.maxLength(100),
+//     ])),
 
+//   });
 
-@Component({
-  selector: 'app-player-list-component',
-  templateUrl: 'player-list.component.html',
-  styleUrls: ['./player-list.component.scss'],
-  providers: [],
-  imports: [
-    MatCardModule,
-    MatFormFieldModule,
-    MatInputModule,
-    FormsModule,
-    MatSelectModule,
-    MatOptionModule,
-    MatRadioModule,
-    MatListModule,
-    RouterLink,
-    MatButtonModule,
-    MatTooltipModule,
-    MatIconModule,
-    MatGridListModule,
-  ],
-})
-export class AddPlayerComponent {
+//   readonly joinGameValidationMessages = {
+//     gameId: [
+//       {type: 'required', message: 'Game ID is required'},
+//       {type: 'minlength', message: 'Game ID must be at least 24 characters long'},
+//       {type: 'maxlength', message: 'Game ID cannot be more than 24 characters long'},
+//       {type: 'pattern', message: 'Game ID must be a 24 character hexadecimal string'},
+//       {type: 'serverError', message: 'Game ID not found -- try a different Game ID'},
+//       {type: 'badRequest', message: 'Bad request -- make sure the Game ID is a valid hexadecimal string'}
+//     ],
+//     playerName: [
+//       {type: 'required', message: 'Player name is required'},
+//       {type: 'minlength', message: 'Name must be at least 2 characters long'},
+//       {type: 'maxlength', message: 'Name cannot be more than 100 characters long'},
+//     ],
+//   };
 
+//   constructor(
+//     private webSocketService: WebSocketService,
+//     private gameService: GameService,
+//     private snackBar: MatSnackBar,
+//     private router: Router) {
+//   }
 
+//   formControlHasError(controlName: string): boolean {
+//     return this.joinGameForm.get(controlName).invalid &&
+//       (this.joinGameForm.get(controlName).dirty || this.joinGameForm.get(controlName).touched);
+//   }
 
+//   getErrorMessage(name: keyof typeof this.joinGameValidationMessages): string {
+//     for(const {type, message} of this.joinGameValidationMessages[name]) {
+//       if (this.joinGameForm.get(name).hasError(type)) {
+//         return message;
+//       }
+//     }
+//     return 'Unknown error';
+//   }
 
-  // Define a signal to hold the player player, initially undefined
-  //playerPlayer = signal<string | undefined>(undefined);
+//   submitForm() {
+//     this.gameService.addPlayer(this.joinGameForm.value.gameId, this.joinGameForm.value.playerName, ).subscribe({
+//       next: () => {
+//         this.snackBar.open(
+//           `Joined game with id: ${this.joinGameForm.value.gameId}`,
+//           null,
+//           { duration: 5000 }
+//         );
+//       },
+//       error: err => {
+//         if (err.status === 400) {
+//           this.snackBar.open(
+//             `Tried to join an illegal game – Error Code: ${err.status}\nMessage: ${err.message}`,
+//             'OK',
+//             { duration: 5000 }
+//           );
+//           this.joinGameForm.controls['gameId'].setErrors({ badRequest: err.message });
+//         } else if (err.status === 500) {
+//           this.snackBar.open(
+//             `The server failed to process your request to join a game. Is the server up? – Error Code: ${err.status}\nMessage: ${err.message}`,
+//             'OK',
+//             { duration: 5000 }
+//           );
+//           this.joinGameForm.controls['gameId'].setErrors({ serverError: err.message });
+//         } else {
+//           this.snackBar.open(
+//             `An unexpected error occurred – Error Code: ${err.status}\nMessage: ${err.message}`,
+//             'OK',
+//             { duration: 5000 }
+//           );
+//         }
+//       },
+//       complete: () => {
+//         this.onPlayerAdd();
+//         this.router.navigate([`/games/${this.joinGameForm.value.gameId}`]);
+//       }
+//     });
+//   }
 
+//   onPlayerAdd() {
+//     const message = {
+//       type: 'ADD_PLAYER',
+//       gameId: this.joinGameForm.value.gameId,
+//       playerName: this.joinGameForm.value.playerName,
+//     };
 
-  // Define a form group for adding a player with validation rules
-  addPlayerForm = new FormGroup({
-    name: new FormControl('', Validators.compose([
-      Validators.required, // Player is required
-      Validators.minLength(2), // Minimum length of 3 characters
-      Validators.maxLength(200) // Maximum length of 200 characters
-    ]))
-  });
+//     this.webSocketService.sendMessage(message);
+//   }
 
-
-
-  // Define validation messages for the player form control
-  readonly addPlayerValidationMessages = {
-    name: [
-      { type: 'required', message: 'Player name is required' },
-      { type: 'minlength', message: 'Player must be at least 2 characters'},
-      { type: 'maxlength', message: 'Player name cannot be more than 200 characters long' }
-    ]
-  };
-
-
-
-  // Inject dependencies: MatSnackBar for displaying messages and Router for navigation
-  constructor(
-    private playerService: PlayerService,
-    private snackBar: MatSnackBar,
-    private router: Router) {
-  }
-
-
-
-  // Check if a form control has an error and has been touched or is dirty
-  formControlHasError(controlName: string): boolean {
-    return this.addPlayerForm.get(controlName).invalid &&
-      (this.addPlayerForm.get(controlName).dirty || this.addPlayerForm.get(controlName).touched);
-  }
-
-
-
-
-  // Get the error message for a specific form control based on validation rules
-  getErrorMessage(controlName: keyof typeof this.addPlayerValidationMessages): string {
-    for (const { type, message } of this.addPlayerValidationMessages[controlName]) {
-      if (this.addPlayerForm.get(controlName).hasError(type)) {
-        return message;
-      }
-    }
-    return 'Unknown error';
-  }
-
-
-  // Handle form submission
-  submitPlayer() {
-    console.log(this.addPlayerForm.value);
-    this.playerService.addPlayer(this.addPlayerForm.value).subscribe({
-      next: (newId) => {
-        this.snackBar.open(
-          `Player added with ID: ${newId} and name: ${this.addPlayerForm.value.name}`,
-          null,
-          { duration: 2000 }
-        );
-        this.router.navigate(['/players/']);
-      },
-
-      error: error => {
-        this.snackBar.open(
-          `Error adding player: ${error.message}`,
-          'OK',
-          { duration: 5000 }
-        );
-      },
-
-
-    });
-  }
-
-
-
-
-
-
-
-}
+// }
--- client/src/app/players/player.ts
+++ client/src/app/players/player.ts
@@ -1,4 +1,4 @@
-export interface Player {
-  _id: string;
-  name: string;
-}
+// export interface Player {
+//   _id: string;
+//   name: string;
+// }
