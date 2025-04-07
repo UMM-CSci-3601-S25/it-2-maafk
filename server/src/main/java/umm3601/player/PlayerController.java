a1c35f0ec4df425df9715f6fb6e70876c948b12d
Mahathir <mosta044@umn.edu>
2025-04-06 20:49:47 -0500

    player components in the server side
    Commented these out because these were unncessary.

--- server/src/main/java/umm3601/player/PlayerController.java
+++ server/src/main/java/umm3601/player/PlayerController.java
@@ -1,64 +1,64 @@
-package umm3601.player;
+// package umm3601.player;
 
-import java.util.ArrayList;
-
-
-import java.util.Map;
+// import java.util.ArrayList;
 
 
-import org.bson.UuidRepresentation;
-import org.mongojack.JacksonMongoCollection;
-
-import com.mongodb.client.MongoDatabase;
+// import java.util.Map;
 
 
-import io.javalin.Javalin;
-import io.javalin.http.Context;
-import io.javalin.http.HttpStatus;
-import umm3601.Controller;
+// import org.bson.UuidRepresentation;
+// import org.mongojack.JacksonMongoCollection;
 
-public class PlayerController implements Controller {
-  private static final String API_PLAYERS = "/api/players";
+// import com.mongodb.client.MongoDatabase;
 
 
-  private final JacksonMongoCollection<Player> playerCollection;
+// import io.javalin.Javalin;
+// import io.javalin.http.Context;
+// import io.javalin.http.HttpStatus;
+// import umm3601.Controller;
 
-  public PlayerController(MongoDatabase database) {
-    playerCollection = JacksonMongoCollection.builder().build(
-        database,
-        "players",
-        Player.class,
-        UuidRepresentation.STANDARD);
-  }
-
-  public void getPlayers(Context ctx) {
-    ArrayList<Player> players = playerCollection.find().into(new ArrayList<>());
-    ctx.json(players);
-  }
+// public class PlayerController implements Controller {
+//   private static final String API_PLAYERS = "/api/players";
 
 
+//   private final JacksonMongoCollection<Player> playerCollection;
 
-  public void addNewPlayer(Context ctx) {
+//   public PlayerController(MongoDatabase database) {
+//     playerCollection = JacksonMongoCollection.builder().build(
+//         database,
+//         "players",
+//         Player.class,
+//         UuidRepresentation.STANDARD);
+//   }
 
-    String body = ctx.body();
-    Player newPlayer = ctx.bodyValidator(Player.class)
-      .check(prm -> prm.name != null && prm.name.length() > 0,
-        "players must have a non-empty value; body was " + body)
-      .get();
+//   public void getPlayers(Context ctx) {
+//     ArrayList<Player> players = playerCollection.find().into(new ArrayList<>());
+//     ctx.json(players);
+//   }
 
-      playerCollection.insertOne(newPlayer);
 
-    ctx.json(Map.of("id", newPlayer._id));
-    ctx.status(HttpStatus.CREATED);
-  }
 
-  @Override
-  public void addRoutes(Javalin server) {
+//   public void addNewPlayer(Context ctx) {
 
-    // Get all players
-    server.get(API_PLAYERS, this::getPlayers);
+//     String body = ctx.body();
+//     Player newPlayer = ctx.bodyValidator(Player.class)
+//       .check(prm -> prm.name != null && prm.name.length() > 0,
+//         "players must have a non-empty value; body was " + body)
+//       .get();
 
-    // Add new players
-    server.post(API_PLAYERS, this::addNewPlayer);
-  }
-}
+//       playerCollection.insertOne(newPlayer);
+
+//     ctx.json(Map.of("id", newPlayer._id));
+//     ctx.status(HttpStatus.CREATED);
+//   }
+
+//   @Override
+//   public void addRoutes(Javalin server) {
+
+//     // Get all players
+//     server.get(API_PLAYERS, this::getPlayers);
+
+//     // Add new players
+//     server.post(API_PLAYERS, this::addNewPlayer);
+//   }
+// }
