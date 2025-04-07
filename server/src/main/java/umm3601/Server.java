e5c0343457ba3e7e4080d3979b904c625345f07a
Armando Valdez <valde148@d.umn.edu>
2025-04-07 09:54:41 -0500

    Testing in back end
    Co-authored-by: Abhineswari Seelam
    <seela031@umn.edu>
    
    Co-authored-by: Mahathir
    <mosta044@umn.edu>
    
    Co-authored-by: Fiona
    <husse412@morris.umn.edu>

--- server/src/main/java/umm3601/Server.java
+++ server/src/main/java/umm3601/Server.java
@@ -12,67 +12,76 @@ import org.bson.UuidRepresentation;
 import io.javalin.Javalin;
 import io.javalin.http.InternalServerErrorResponse;
 
-
+/**
+ * The main server class that configures and starts the Javalin server
+ * with MongoDB connection and controller routes.
+ */
 public class Server {
 
-  private static final int SERVER_PORT = 4567;
-  private final MongoClient mongoClient;
-  private Controller[] controllers;
+    private static final int SERVER_PORT = 4567;
+    private final MongoClient mongoClient;
+    private final Controller[] controllers;
 
-  public Server(MongoClient mongoClient, Controller[] controllers) {
-    this.mongoClient = mongoClient;
-
-    this.controllers = Arrays.copyOf(controllers, controllers.length);
-  }
-
-  static MongoClient configureDatabase(String mongoAddr) {
-    MongoClient mongoClient = MongoClients.create(MongoClientSettings
-      .builder()
-      .applyToClusterSettings(builder -> builder.hosts(Arrays.asList(new ServerAddress(mongoAddr))))
-      .uuidRepresentation(UuidRepresentation.STANDARD)
-      .build());
-
-    return mongoClient;
-  }
-
-  void startServer() {
-    Javalin javalin = configureJavalin();
-    setupRoutes(javalin);
-    javalin.start(SERVER_PORT);
-  }
-
-  private Javalin configureJavalin() {
-  
-    Javalin server = Javalin.create(config ->
-      config.bundledPlugins.enableRouteOverview("/api")
-    );
-
-    // Configure the MongoDB client and the Javalin server to shut down gracefully.
-    configureShutdowns(server);
-
-    server.exception(Exception.class, (e, ctx) -> {
-      throw new InternalServerErrorResponse(e.toString());
-    });
-
-    return server;
-  }
-
- 
-  private void configureShutdowns(Javalin server) {
-   
-    Runtime.getRuntime().addShutdownHook(new Thread(server::stop));
-  
-    server.events(event -> {
-      event.serverStartFailed(mongoClient::close);
-      event.serverStopped(mongoClient::close);
-    });
-  }
-
-  
-  private void setupRoutes(Javalin server) {
-   
-    for (Controller controller : controllers) {
-      controller.addRoutes(server);
+    /**
+     * Constructs a new Server instance.
+     *
+     * @param mongoClient the MongoDB client
+     * @param controllers the array of controllers to handle routes
+     */
+    public Server(MongoClient mongoClient, Controller[] controllers) {
+        this.mongoClient = mongoClient;
+        this.controllers = Arrays.copyOf(controllers, controllers.length);
     }
-  }
+
+    /**
+     * Configures the MongoDB client with the given server address.
+     *
+     * @param mongoAddr the MongoDB server address
+     * @return configured MongoClient
+     */
+    public static MongoClient configureDatabase(String mongoAddr) {
+        return MongoClients.create(MongoClientSettings
+            .builder()
+            .applyToClusterSettings(builder ->
+                builder.hosts(Arrays.asList(new ServerAddress(mongoAddr))))
+            .uuidRepresentation(UuidRepresentation.STANDARD)
+            .build());
+    }
+
+    /**
+     * Starts the Javalin server.
+     */
+    public void startServer() {
+        Javalin javalin = configureJavalin();
+        setupRoutes(javalin);
+        javalin.start(SERVER_PORT);
+    }
+
+    private Javalin configureJavalin() {
+        Javalin server = Javalin.create(config ->
+            config.bundledPlugins.enableRouteOverview("/api")
+        );
+
+        configureShutdowns(server);
+
+        server.exception(Exception.class, (e, ctx) -> {
+            throw new InternalServerErrorResponse(e.toString());
+        });
+
+        return server;
+    }
+
+    private void configureShutdowns(Javalin server) {
+        Runtime.getRuntime().addShutdownHook(new Thread(server::stop));
+        server.events(event -> {
+            event.serverStartFailed(mongoClient::close);
+            event.serverStopped(mongoClient::close);
+        });
+    }
+
+    private void setupRoutes(Javalin server) {
+        for (Controller controller : controllers) {
+            controller.addRoutes(server);
+        }
+    }
 }
--- server/src/main/java/umm3601/player/Player.java
+++ server/src/main/java/umm3601/player/Player.java
@@ -1,36 +1,36 @@
-// package umm3601.player;
+package umm3601.player;
 
-// import org.mongojack.Id;
-// import org.mongojack.ObjectId;
+import org.mongojack.Id;
+import org.mongojack.ObjectId;
 
+/**
+ * Represents a player in the system.
+ */
+@SuppressWarnings({"VisibilityModifier"})
+public class Player {
 
+  @ObjectId @Id
+  @SuppressWarnings({"MemberName"})
+  public String _id;
 
-// @SuppressWarnings({"VisibilityModifier"})
-// public class Player {
+  public String name;
 
-//   @ObjectId @Id
-//   @SuppressWarnings({"MemberName"})
-//   public String _id;
+  @Override
+  public boolean equals(Object obj) {
+    if (!(obj instanceof Player)) {
+      return false;
+    }
+    Player other = (Player) obj;
+    return _id.equals(other._id);
+  }
 
-//   public String name;
+  @Override
+  public int hashCode() {
+    return _id.hashCode();
+  }
 
-//   @Override
-//   public boolean equals(Object obj) {
-//     if (!(obj instanceof Player)) {
-//       return false;
-//     }
-//     Player other = (Player) obj;
-//     return _id.equals(other._id);
-//   }
-
-//   @Override
-//   public int hashCode() {
-//     return _id.hashCode();
-//   }
-
-//   @Override
-//   public String toString() {
-//     return name;
-//   }
-
-// }
+  @Override
+  public String toString() {
+    return name;
+  }
+}
