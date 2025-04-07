c1d30c020be35a3f5a06869c6afe42a00de7f72c
Mahathir <mosta044@umn.edu>
2025-04-06 20:46:41 -0500

    removed comments
    removed a comment from round.ts

--- client/src/app/game/round.ts
+++ client/src/app/game/round.ts
@@ -2,6 +2,6 @@ export interface Round {
     _id: string;
     players: string[];
     responses: Response[];
-    judge: string; // will be one of the strings from players
+    judge: string; 
   }
   
